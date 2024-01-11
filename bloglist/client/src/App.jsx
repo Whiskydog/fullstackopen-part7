c983a9246435763199f3c900be2aca8853e7399d
Whiskydog <alan.varde@gmail.com>
2024-01-11 06:21:53 -0300

    Solve exercise 7.17

--- bloglist/client/src/App.jsx
+++ bloglist/client/src/App.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { Outlet } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadUser, logOffUser } from './reducers/user';
+import { loadUser } from './reducers/user';
 import { loadBlogs } from './reducers/blogs';
 import { loadUsers } from './reducers/users';
 import Notification from './components/Notification';
 import Login from './routes/Login';
+import Menu from './components/Menu';
 
 const App = () => {
   const user = useSelector((state) => state.user);
@@ -17,21 +18,13 @@ const App = () => {
     dispatch(loadUsers());
   }, [dispatch]);
 
-  const handleLogout = () => {
-    dispatch(logOffUser());
-  };
-
-  if (!user) {
-    return <Login />;
-  }
+  if (!user) return <Login />;
 
   return (
     <div>
+      <Menu />
       <h2>Blogs</h2>
       <Notification />
-      <p>
-        {user.name} logged in <button onClick={handleLogout}>logout</button>
-      </p>
       <Outlet />
     </div>
   );
